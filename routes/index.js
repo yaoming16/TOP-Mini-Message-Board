d80a88b6aa19d51c5970fdbf695db75fa0a9c99e
Pablo Perez <pabloperezzz16@gmail.com>
2026-03-02 21:10:13 +0000

    Changed screenshots for readme and initial example cards

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ const indexRouter = Router();
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
+    text: "In the middle of the journey of our life, I came to myself, in a dark wood, where the direct way was lost. It is a hard thing to speak of, how wild, harsh and impenetrable that wood was, so that thinking of it recreates the fear. It is scarcely less bitter than death: but, in order to tell of the good that I found there, I must tell of the other things I saw there.",
+    user: "Dante Alighieri",
     added: new Date(),
     id: crypto.randomUUID(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
+    text: "In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing.",
+    user: "Miguel de Cervantes",
     added: new Date(),
     id: crypto.randomUUID(),
   },
